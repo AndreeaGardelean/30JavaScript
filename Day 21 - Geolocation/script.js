a9e4809a7717b19417e27bfa1c2026383acb7981
Andreea Gardelean <andreea.gardelean.2020@live.rhul.ac.uk>
2024-09-03 09:42:16 +0100

    Updating speed value text

--- Day 21 - Geolocation/script.js
+++ Day 21 - Geolocation/script.js
@@ -2,10 +2,8 @@ const arrow = document.querySelector('.arrow');
 const speed = document.querySelector('.speed-value');
 
 function updateData(data) {
-  speed.innerHTML = data.coords.speed || 0;
-  const [x, y] = [data.longitude, data.latitude];
-  arrow.style.transform = `rotate(${data.coords.heading})deg`;
-  console.log(data.coords.heading)
+  speed.textContent = data.coords.speed || 0;
+  arrow.style.transform = `rotate(${data.coords.heading}deg)`;
 };
 
 navigator.geolocation.watchPosition(data => { updateData(data) }, (err) => {
